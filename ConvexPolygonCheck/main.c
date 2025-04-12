0cbbceb51577759f1d7cd1437a9bf621d28d0c1e
Mihajlo Vidanović <vidanovicmihajlo07@gmail.com>
2025-04-12 12:38:43 +0200

    changed algorithm for IsPolygonConvex

--- ConvexPolygonCheck/main.c
+++ ConvexPolygonCheck/main.c
@@ -6,31 +6,36 @@
 // current program mode
 typedef enum mode { PlaceVertex = 0, ConnectStartingVertex = 1 } mode;
 
+// vector2 but with double's
+typedef struct Vector2d {
+    double x, y;
+} Vector2d;
+
 // doubly linked list node
-typedef struct Vector2Node {
-    Vector2 value;
-    struct Vector2Node *next, *prev;
-} Vector2Node;
+typedef struct Vector2dNode {
+    Vector2d value;
+    struct Vector2dNode *next, *prev;
+} Vector2dNode;
 
 // function to subtract a - b
-Vector2 SubtractVector(const Vector2 a, const Vector2 b) {
-    return (Vector2){a.x - b.x, a.y - b.y};
+Vector2d SubtractVector(const Vector2d a, const Vector2d b) {
+    return (Vector2d){a.x - b.x, a.y - b.y};
 }
 
 // simple function to check whether xy is within circle pos, r
-bool WithinCircle(const Vector2 xy, const Vector2 pos, const float r) {
+bool WithinCircle(const Vector2d xy, const Vector2d pos, const float r) {
     // pythagorean theorem
-    if (sqrtf((xy.x - pos.x) * (xy.x - pos.x) + (xy.y - pos.y) * (xy.y - pos.y)) <= r) {
+    if (sqrt((xy.x - pos.x) * (xy.x - pos.x) + (xy.y - pos.y) * (xy.y - pos.y)) <= r) {
         return true;
     }
     return false;
 }
 
 // a function for freeing the polygon data
-void DeletePolygon(Vector2Node* polygon, int polygonSize) {
+void DeletePolygon(Vector2dNode* polygon, int polygonSize) {
     while (polygonSize--) {
         // simple iteration through the polygon
-        Vector2Node *p = polygon;
+        Vector2dNode *p = polygon;
         // without the 2 lines below, the program crashes
         polygon->prev->next = polygon->next;
         polygon->next->prev = polygon->prev;
@@ -39,52 +44,47 @@ void DeletePolygon(Vector2Node* polygon, int polygonSize) {
     }
 }
 
-float FindAngle(Vector2 vector) {
-    vector.x = vector.x / sqrtf(vector.x * vector.x + vector.y * vector.y);
-    vector.y = vector.y / sqrtf(vector.x * vector.x + vector.y * vector.y);
+double FindAngle(Vector2d vector) {
+    vector.x = vector.x / sqrt(vector.x * vector.x + vector.y * vector.y);
+    vector.y = vector.y / sqrt(vector.x * vector.x + vector.y * vector.y);
     if (vector.y > 0) { // quadrant 1 and 2
-        return atan2f(vector.y, vector.x);
+        return atan2(vector.y, vector.x);
     }
-    return 2*PI + atan2f(vector.y, vector.x);
+    return 2.0*PI + atan2(vector.y, vector.x);
 }
 
-bool IsPolygonConvex(const Vector2Node* polygon, int polygonSize) {
-    /* There are many methods to do this, but the method i will use is:
-     * -Go from vertex to vertex
-     * -Find the vector between the two vertices
-     * -Find the angle of this vector
-     * -Compare the angle to the previous two
-     * -If the previous angle is not within the bounds of the current angle and one before the previous one,
-     * that is a cavity and the polygon is concave.
-     * -Repeat polygonSize times. */
+bool IsPolygonConvex(Vector2dNode* polygon, int polygonSize) {
+    /* Since the method outlined in my previous commit is buggy, i have decided to change it to another.
+     * -Find the edge with the smallest angle value.
+     * -Iterate through every next edge.
+     * -If the angle is smaller than the current angle, the polygon must be concave.
+     */
     // Every triangle/line/vertex is convex
     if (polygonSize <= 3) {
         return true;
     }
-    // dumb trick to make clangd ignore that const could be added to the declarations within the while loop,
-    // since adding const makes glitches
-    float angle1 = 0.0f, angle2 = 0.0f, angle3 = 0.0f;
-    while (polygonSize--) {
-        const Vector2 edge1 = SubtractVector(polygon->prev->value, polygon->prev->prev->value);
-        const Vector2 edge2 = SubtractVector(polygon->value, polygon->prev->value);
-        const Vector2 edge3 = SubtractVector(polygon->next->value, polygon->value);
-        // find all the angles of these vectors
-        angle1 = FindAngle(edge1);
-        angle2 = FindAngle(edge2);
-        angle3 = FindAngle(edge3);
-        // well... i can't explain it in a comment without you drawing it out and seeing for yourself :)
-        if (angle1 >= angle3) {
-            if (!(angle2 < angle1) && !(angle2 > angle3)) {
+    Vector2dNode *currAngle = polygon;
+    for (Vector2dNode* i = polygon->next; i != polygon; i = i->next) {
+        if (FindAngle(SubtractVector(i->value, i->prev->value)) < FindAngle(SubtractVector(currAngle->value, currAngle->prev->value))) {
+            currAngle = i;
+        }
+    }
+    polygonSize--;
+    if (FindAngle(SubtractVector(currAngle->next->value, currAngle->value)) > FindAngle(SubtractVector(currAngle->prev->value, currAngle->prev->prev->value))) {
+        while (--polygonSize) {
+            if (FindAngle(SubtractVector(currAngle->prev->value, currAngle->prev->prev->value)) < FindAngle(SubtractVector(currAngle->value, currAngle->prev->value))) {
                 return false;
             }
+            currAngle = currAngle->prev;
         }
-        // and this is just the normal case
-        else {
-            if (angle2 > angle3 || angle2 < angle1) {
+    }
+    else {
+        while (--polygonSize) {
+            if (FindAngle(SubtractVector(currAngle->next->value, currAngle->value)) < FindAngle(SubtractVector(currAngle->value, currAngle->prev->value))) {
                 return false;
             }
+            currAngle = currAngle->next;
         }
-        polygon = polygon->next;
     }
     return true;
 }
@@ -95,12 +95,12 @@ int main() {
     InitWindow(800, 600, "Check whether a polygon is convex");
 
     // this is our polygon
-    Vector2Node* polygon = NULL;
+    Vector2dNode* polygon = NULL;
     int polygonSize = 0;
 
     // these are temporary (?) variables that are used for selecting/adding vertices
-    Vector2Node* selectedVertex = NULL;
-    Vector2Node* tmp = NULL;
+    Vector2dNode* selectedVertex = NULL;
+    Vector2dNode* tmp = NULL;
 
     // variable that indicates what mode the program is in
     mode m = PlaceVertex;
@@ -122,7 +122,7 @@ int main() {
                     // a new vertex is to be placed and added to the polygon
                     if (polygonSize == 0) {
                         // this is the first vertex of the polygon
-                        polygon = (Vector2Node*)malloc(sizeof(Vector2Node));
+                        polygon = (Vector2dNode*)malloc(sizeof(Vector2dNode));
                         polygon->value.x = (float)x;
                         polygon->value.y = (float)y;
                         // homemade doubly linked list
@@ -133,7 +133,7 @@ int main() {
                         // we are adding a new vertex
                         // make the polygon vertices be in a loop structure
                         // the assumption is the polygon vertices are already in a loop structure
-                        if (WithinCircle((Vector2){(float)x, (float)y}, selectedVertex->value, 5.0f)) {
+                        if (WithinCircle((Vector2d){(float)x, (float)y}, selectedVertex->value, 5.0f)) {
                             // the same vertex was selected again, unselect it
                             selectedVertex = NULL;
                             m = ConnectStartingVertex;
@@ -142,7 +142,7 @@ int main() {
                         // tmp is the vertex that selectedVertex was originally connected to,
                         // we are going to connect the new vertex to selectedVertex and tmp
                         tmp = selectedVertex->next;
-                        selectedVertex->next = (Vector2Node*)malloc(sizeof(Vector2Node));
+                        selectedVertex->next = (Vector2dNode*)malloc(sizeof(Vector2dNode));
                         // so that the doubly linked list is preserved
                         tmp->prev = selectedVertex->next;
                         selectedVertex->next->value.x = (float)x;
@@ -157,15 +157,15 @@ int main() {
                     break;
                 case ConnectStartingVertex:
                     // this statement is necessary because the for loop skips over polygon
-                    if (WithinCircle((Vector2){(float)x, (float)y}, (Vector2){polygon->value.x, polygon->value.y}, 5.0f)) {
+                    if (WithinCircle((Vector2d){(double)x, (double)y}, (Vector2d){polygon->value.x, polygon->value.y}, 5.0f)) {
                         selectedVertex = polygon;
                         m = PlaceVertex;
                         break;
                     }
                     // checking each vertex to see if any are selected
                     if (polygon != NULL) {
-                        for (Vector2Node* i = polygon->next; i != polygon; i = i->next) {
-                            if (WithinCircle((Vector2){(float)x, (float)y}, (Vector2){i->value.x, i->value.y}, 5.0f)) {
+                        for (Vector2dNode* i = polygon->next; i != polygon; i = i->next) {
+                            if (WithinCircle((Vector2d){(double)x, (double)y}, (Vector2d){i->value.x, i->value.y}, 5.0f)) {
                                 selectedVertex = i;
                                 m = PlaceVertex;
                                 break;
@@ -180,21 +180,21 @@ int main() {
             const int x = GetMouseX(), y = GetMouseY();
 
             if (polygon != NULL && polygonSize > 0) { // because of clangd screaming at me...
-                if (WithinCircle((Vector2){(float)x, (float)y}, (Vector2){polygon->value.x, polygon->value.y}, 5.0f)) {
+                if (WithinCircle((Vector2d){(double)x, (double)y}, polygon->value, 5.0f)) {
                     // vertex found
                     polygon->prev->next = polygon->next;
                     polygon->next->prev = polygon->prev;
-                    Vector2Node* p = polygon;
+                    Vector2dNode* p = polygon;
                     polygon = polygon->next;
                     free(p);
                     polygonSize--;
                 }
-                for (Vector2Node* i = polygon->next; i != polygon; i = i->next) {
-                    if (WithinCircle((Vector2){(float)x, (float)y}, (Vector2){i->value.x, i->value.y}, 5.0f)) {
+                for (Vector2dNode* i = polygon->next; i != polygon; i = i->next) {
+                    if (WithinCircle((Vector2d){(float)x, (float)y}, i->value, 5.0f)) {
                         // vertex found
                         i->prev->next = i->next;
                         i->next->prev = i->prev;
-                        Vector2Node* p = i;
+                        Vector2dNode* p = i;
                         i = i->next;
                         free(p);
                         polygonSize--;
@@ -217,29 +217,29 @@ int main() {
                 // required because the for loop skips over polygon
                 if(polygon != NULL && selectedVertex == polygon) { // because of clangd screaming at me...
                     // if polygon is selected, draw it red
-                    DrawCircleV(polygon->value, 5.0f, RED);
+                    DrawCircle(polygon->value.x, polygon->value.y, 5.0f, RED);
                 }
                 else if (polygon != NULL) { // because of clangd screaming at me...
-                    DrawCircleV(polygon->value, 5.0f, WHITE);
+                    DrawCircle(polygon->value.x, polygon->value.y, 5.0f, WHITE);
                 }
                 // draw each vertex and a line connecting them
                 if (polygon != NULL) { // because of clangd screaming at me...
                     // iteration through the polygon
-                    for (const Vector2Node* i = polygon->next; i != polygon; i = i->next) {
+                    for (const Vector2dNode* i = polygon->next; i != polygon; i = i->next) {
                         if(selectedVertex == i) {
                             // if i is selected, draw it red
-                            DrawCircleV(i->value, 5.0f, RED);
+                            DrawCircle(i->value.x, i->value.y, 5.0f, RED);
                         }
                         else {
-                            DrawCircleV(i->value, 5.0f, WHITE);
+                            DrawCircle(i->value.x, i->value.y, 5.0f, WHITE);
                         }
                         // draw a line between the current vertex and the previous one
-                        DrawLineEx(i->prev->value, i->value, 3.0f, WHITE);
+                        DrawLineEx((Vector2){i->prev->value.x, i->prev->value.y}, (Vector2){i->value.x, i->value.y}, 3.0f, WHITE);
                     }
                 }
                 if (polygon != NULL) { // because of clangd screaming at me...
                     // the final line between polygon->prev and polygon, skipped in for loop
-                    DrawLineEx(polygon->prev->value, polygon->value, 3.0f, WHITE);
+                    DrawLineEx((Vector2){polygon->prev->value.x, polygon->prev->value.y}, (Vector2){polygon->value.x, polygon->value.y}, 3.0f, WHITE);
                 }
             }
 
